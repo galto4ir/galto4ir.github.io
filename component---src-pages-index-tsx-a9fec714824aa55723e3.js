c0af900aeb1696b04c594b8a00df89afdd149a50
Galt Davaa <galt@steppelink.mn>
2022-07-21 12:05:25 +0800

    Updates

---
+++ component---src-pages-index-tsx-a9fec714824aa55723e3.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgalto4ir_github_io=self.webpackChunkgalto4ir_github_io||[]).push([[691],{7200:function(e,t,l){l.r(t);var a=l(7294);t.default=function(e){return a.createElement("main",null,a.createElement("h3",null,a.createElement("code",null,"GaltOchir Davaa ","<galto4ir@gmail.com>")))}}}]);
+//# sourceMappingURL=component---src-pages-index-tsx-a9fec714824aa55723e3.js.map
