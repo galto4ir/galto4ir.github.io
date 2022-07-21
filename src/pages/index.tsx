44d0de469f0d2e107d24943d05619cd6d286a837
Galt Davaa <galt@steppelink.mn>
2022-07-21 11:59:35 +0800

    init

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+
+interface IProps {}
+
+const IndexPage = (props: IProps) => {
+  return (
+    <main>
+      <h3>
+        <code>GaltOchir Davaa {"<galto4ir@gmail.com>"}</code>
+      </h3>
+    </main>
+  );
+};
+
+export default IndexPage;
